1039fb1f918de5228574ea41240daf48958206bb
Oscar Perilla <operilladominguez@gmail.com>
2020-10-21 17:44:05 -0500

    primer commit desde kraken

--- hola.py
+++ hola.py
@@ -1,8 +1,5 @@
 print("hola mundo, este es un mensaje de prueba de un primer commit")
-<<<<<<< HEAD
-
 print("Hola en el master")
 print("Modificación en rama")
-=======
 print("Que ondaaaaa")
->>>>>>> d6ab2abd61348d92ab06e0d7e2de6be321989271
+print("Prueba para git kraken")
